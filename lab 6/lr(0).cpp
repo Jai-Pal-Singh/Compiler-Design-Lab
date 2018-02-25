ae5dcb1950f8d70ff5e22f954e763d85f90e7493
Jai Pal Singh <jai.pal.2013@gmail.com>
2018-02-25 18:40:18 +0530

    lab 6 assignment after small changes

--- lab 6/lr(0).cpp
+++ lab 6/lr(0).cpp
@@ -7,16 +7,20 @@ set<string> productions;
 set<string>::iterator prodIterator;
 vector< vector< pair<string,string> > > items;
 vector< vector< pair<string,string> > >::iterator it;
-vector< pair<string,string> > closure;
+vector< pair<string,string> > closure, vec, quetop;
 vector< pair<string,string> >::iterator cl;
 string startSymbol;
+queue <vector< pair<string,string> > > nextItem;
+
 void displayProduction();
 void displayitems();
 void computeitems();
+vector< pair<string,string> > createItemSet(vector< pair<string,string> > inp);
+void findGotoSet(vector< pair<string,string> > inp);
 
 int main(void){
     FILE *input = freopen("lr(0)Input.txt","r",stdin);
-//	FILE *output = freopen("lr(0)Output.txt","w",stdout);
+	FILE *output = freopen("lr(0)Output.txt","w",stdout);
     int inputLength, terLen;
     string str;
     cin >> startSymbol;
@@ -72,8 +76,95 @@ void displayitems(){
 }
 
 void computeitems(){
-    it = items.begin();
-    cl = closure.begin();
-    cl = closure.insert(cl, make_pair(startSymbol+"`","."+startSymbol));
-    it = items.insert(it, closure);
+    closure.push_back(make_pair(startSymbol+"'",startSymbol));
+    nextItem.push(closure);
+    while(!nextItem.empty()){
+        int itemAlreadyExist = 0;
+		quetop = nextItem.front();
+        closure = createItemSet(nextItem.front());
+        for(it = items.begin(); it != items.end(); it++){
+            if(closure == *it){
+                itemAlreadyExist = 1;
+            }
+        }
+        if(itemAlreadyExist==0){
+            items.push_back(closure);
+			findGotoSet(closure);
+        }
+        nextItem.pop();
+    }
+}
+
+vector< pair<string,string> > createItemSet(vector< pair<string,string> > inp){
+	string visited = "";
+    for(int k = 0 ; k < inp.size(); k++){
+        string str = inp[k].second;
+        int i = str.find(".");
+        if(i==str.npos){                        //. not found. Starting condition
+            inp[k].second = "."+str;
+            str = inp[k].second;
+            i=1;
+        }
+        else if(i+1==str.length()){          //. at the end of production
+            continue;
+        }
+        else{                              //. in middle of string, Advancing .
+            str[i] = str[i+1];
+            str[i+1] = '.';
+            inp[k].second = str;
+			i += 2;
+        }
+        if(str[i]=='e'){                    //epsilon condition
+            str.erase(i);
+            inp[k].second = str;
+            continue;
+        }
+        else if(str[i]>='a'&&str[i]<='z'&&str[i]!='e'){             //terminal condition
+			continue;
+        }
+        else if(str[i]>='A'&&str[i]<='Z'&&str[i]!='e'){             //non-terminal condition
+            gr = grammar.find(string(1,str[i]));
+            if(gr == grammar.end()){
+                cout << "Error : " << str[i] <<" is not presented in grammar.\n";
+                break;
+            }
+			int l = visited.find(str[i]);
+            if(visited[l]== str[i]){
+                continue;
+            }
+            visited.push_back(str[i]);
+            productions = gr->second;
+            for(prodIterator = productions.begin(); prodIterator != productions.end(); ++prodIterator){
+                pair<string, string> p = make_pair(gr->first,*prodIterator);
+                inp.push_back(p);
+            }
+        }
+    }
+    return inp;
+}
+
+void findGotoSet(vector< pair<string,string> > inp){
+    string visited = "";
+    for(int i=0; i<inp.size(); i++){
+        vec.clear();
+        string s1 = inp[i].second;
+        int pos1 = s1.find(".");
+        int l = visited.find(s1[pos1+1]);
+		if(pos1+1==s1.length()){
+			continue;
+		}
+        if(visited[l]== s1[pos1+1]){
+            continue;
+        }
+        visited.push_back(s1[pos1+1]);
+        vec.push_back(make_pair(inp[i].first,inp[i].second));
+        for(int j=i+1; j<inp.size(); j++){
+            string s2 = inp[j].second;
+            int pos2 = s2.find(".");
+            if(s1[pos1+1]==s2[pos2+1]){
+                vec.push_back(make_pair(inp[j].first,inp[j].second));
+            }
+        }
+        nextItem.push(vec);
+    }
 }
